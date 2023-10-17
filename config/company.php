929fb4f91c6d5a8cca3e323b05fc17dd7bdc8093
rivaalms <rvalms@icloud.com>
2023-10-18 00:41:11 +0700

    build(config): add company config

---
+++ config/company.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+   'name' => env('COMPANY_NAME', ''),
+   'address' => env('COMPANY_ADDRESS', ''),
+   'email' => env('COMPANY_EMAIL', ''),
+   'phone' => env('COMPANY_PHONE', '')
+];
