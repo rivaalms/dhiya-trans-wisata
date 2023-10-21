199fbc202f5df22cf2e08bbc762ffb2720d05e7a
rivaalms <rvalms@icloud.com>
2023-10-21 10:36:35 +0700

    refactor: users migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,10 +19,10 @@ class DatabaseSeeder extends Seeder
    {
       // \App\Models\User::factory(10)->create();
 
-      // \App\Models\User::factory()->create([
-      //     'name' => 'Test User',
-      //     'email' => 'test@example.com',
-      // ]);
+      \App\Models\User::factory()->create([
+         'name' => 'Admin',
+         'email' => 'admin@example.com',
+      ]);
 
       Vehicle::factory(10)->create();
       Destination::factory(10)->create();
