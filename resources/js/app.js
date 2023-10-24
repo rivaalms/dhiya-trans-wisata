6f17b1de3c92bace9e73d27b9a44e5071cc42fcc
Riva Almero <riva@dev.netkromsolution.com>
2023-10-24 16:37:00 +0700

    feat: lazy load pages

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,14 +2,14 @@ import './bootstrap';
 import '../css/app.css';
 
 import { createApp, h } from 'vue'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import { createInertiaApp } from '@inertiajs/vue3'
 
-createInertiaApp({
-   resolve: name => {
-      const pages = import.meta.glob('./pages/**/*.vue', { eager: true })
-      return pages[`./pages/${name}.vue`]
-   },
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
 
+createInertiaApp({
+   title: (title) => title ? `${title} - ${appName}` : `${appName}`,
+   resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob('./pages/**/*.vue')),
    setup({ el, App, props, plugin }) {
       createApp({ render: () => h(App, props) })
          .use(plugin)
