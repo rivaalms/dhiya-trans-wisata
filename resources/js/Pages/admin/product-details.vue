2458805417440547a39bc817067c0dfd9bb9b7c8
Riva Almero <riva@dev.netkromsolution.com>
2023-11-09 11:57:07 +0700

    feat: add cover tag & back button

--- resources/js/Pages/admin/product-details.vue
+++ resources/js/Pages/admin/product-details.vue
@@ -1,17 +1,23 @@
 <template>
 <admin-layout :title="product?.name || 'Produk'">
-   <div class="flex justify-end mb-4">
+   <div class="flex justify-between mb-4">
+      <Link
+         href="/admin/products"
+         class="py-2 px-2 rounded hover:bg-gray-50 hover:shadow-md hover:shadow-gray-200 transition-all duration-300"
+      >
+         Kembali
+      </Link>
       <button class="rounded py-2 px-4 bg-blue-500 text-white hover:bg-blue-600 transition-all duration-300" @click.stop="showModal('new')">
          Tambah Gambar
       </button>
    </div>
-   <div v-if="product" class="grid grid-cols-4 gap-4 place-items-start">
+   <div v-if="product?.images && product?.images.length > 0" class="grid grid-cols-4 gap-4 place-items-start">
       <template v-for="image in product.images">
          <div class="relative">
             <img :src="image.path" alt="" class="rounded-lg show hover:shadow-lg bg-white transition-all">
 
             <div class="absolute inset-x-0 top-0 flex justify-end gap-1 p-2">
-               <button class="bg-amber-500/70 px-3 py-1 text-xs text-white font-semibold rounded hover:bg-amber-500 hover:shadow hover:shadow-amber-300 transition-all duration-300" @click.stop="showModal(image.uuid)">
+               <button class="bg-amber-500/70 px-3 py-1 text-xs text-white font-semibold rounded hover:bg-amber-500 hover:shadow hover:shadow-amber-300 transition-all duration-300" @click.stop="showModal(image.uuid, true)">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="w-4 h-4">
                      <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                   </svg>
@@ -22,8 +28,17 @@
                   </svg>
                </button>
             </div>
+
+            <div class="absolute top-2 left-2">
+               <span v-if="image.is_cover" class="bg-blue-500/70 px-3 py-1 text-xs text-white font-semibold rounded hover:bg-blue-500 hover:shadow-md hover:shadow-blue-400 transition-all duration-300">
+                  Cover
+               </span>
+            </div>
          </div>
       </template>
+   </div>
+   <div v-else class="grid grid-cols-1 place-items-center">
+      Tidak ada gambar
    </div>
 </admin-layout>
 
@@ -31,6 +46,7 @@
    :show="modal.show"
    :id="modal.id"
    :slug="props.slug"
+   :is-edit="modal.isEdit"
    :callback="fetchData"
    :errors="props.errors"
    @close="clearModal"
@@ -41,7 +57,7 @@
 import AdminLayout from '@/layouts/admin.vue'
 import imageFormModal from '@/components/modal/image-form.vue'
 import { ref } from 'vue'
-import { router } from '@inertiajs/vue3';
+import { router, Link } from '@inertiajs/vue3'
 
 const props = defineProps({
    slug: String,
@@ -53,13 +69,15 @@ const product = ref(null)
 const modal = ref({
    show: false,
    id: '',
-   slug: props.slug
+   slug: props.slug,
+   isEdit: false
 })
 
-const showModal = (id) => {
+const showModal = (id, isEdit = false) => {
    modal.value = {
       id,
       show: true,
+      isEdit,
    }
 }
 
